416e2eaa76fafce05a9593b93f0b81c4c8e04f18
Alfus Inigo Jaganathan <ajaganathan@pivotals-MBP.frontier.com>
2018-07-26 15:52:16 -0400

    Made url encoding to none

--- src/main/java/com/example/demo/AppConfiguration.java
+++ src/main/java/com/example/demo/AppConfiguration.java
@@ -1,21 +1,34 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.http.client.HttpClient;
+import org.springframework.cloud.config.server.environment.VaultEnvironmentProperties;
+import org.springframework.cloud.config.server.environment.VaultEnvironmentRepositoryFactory;
+import org.springframework.cloud.config.server.support.HttpClientSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class AppConfiguration {
-	
-
 	@Bean
-	public RestTemplate createRestTemplate() {
-		RestTemplate restTemplate = new RestTemplate();
-		DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
-		defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
-		restTemplate.setUriTemplateHandler(defaultUriBuilderFactory);
-		return restTemplate;
-	}
+    @Primary
+    public VaultEnvironmentRepositoryFactory.VaultRestTemplateFactory myRestTemplateFactory() throws Exception {
+        return new MyVaultRestTemplateFactory();
+    }
+
+    class MyVaultRestTemplateFactory implements VaultEnvironmentRepositoryFactory.VaultRestTemplateFactory {
+
+        @Override
+        public RestTemplate build(VaultEnvironmentProperties environmentProperties) throws Exception {
+            HttpClient httpClient = HttpClientSupport.builder(environmentProperties).build();
+            RestTemplate rest =  new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+            DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
+            defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+            rest.setUriTemplateHandler(defaultUriBuilderFactory);
+            return rest;
+        }
+    }
 }
